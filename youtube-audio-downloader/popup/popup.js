c56187d35559a97cb8531d1d894fa6a942a0d6f3
Spacery <msa.mt314@gmail.com>
2025-08-31 16:14:42 -0400

    feat(js): Set up initial popup script and element reference

---
+++ youtube-audio-downloader/popup/popup.js
@@ -0,0 +1,9 @@
+const videoTitleElement = document.getElementById('videoTitle');
+const videoLengthElement = document.getElementById('videoLength');
+const downloadBtn = document.getElementById('downloadBtn');
+const cancelBtn = document.getElementById('cancelBtn');
+const versionElement = document.getElementById('version');
+
+downloadBtn.addEventListener('click', () => {
+    console.log("Download button clicked");
+});
