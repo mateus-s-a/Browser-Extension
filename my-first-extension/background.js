fd58b532bc963e01bab2651fc9d894946fd01546
Spacery <msa.mt314@gmail.com>
2025-08-31 15:38:39 -0400

    feat: complete cross-browser extension build

--- my-first-extension/background.js
+++ my-first-extension/background.js
@@ -1,5 +1,7 @@
+importScripts('/libs/browser-polyfill.min.js');
+
 // This tells the browser to listen for the 'onInstalled' event
 // When the event happens, it runs the function
 browser.runtime.onInstalled.addListener(() => {
-    console.log("Extension installed.");                                // Message's will be sent to the bg scrip'ts console
+    console.log("Extension installed");                                // Message's will be sent to the bg scrip'ts console
 });
--- my-first-extension/popup/popup.js
+++ my-first-extension/popup/popup.js
@@ -1,9 +1,9 @@
 const title = document.getElementById('title');             // Gets the 'title' h1 reference
 
-async function handleClick() {
-    let tabs = await browser.tabs.query({ active: true, currentWindow: true });
+async function handleClick() {                                                          // 'async' function to have 'await'
+    let tabs = await browser.tabs.query({ active: true, currentWindow: true });         // 1. Ask the browser for the active tab
 
-    if (tabs.length > 0) {
+    if (tabs.length > 0) {                                                              // 2. If it found one, send a message directly to it
         let activeTab = tabs[0];
         browser.tabs.sendMessage(activeTab.id, {
             action: "changeColor"
@@ -11,4 +11,4 @@ async function handleClick() {
     }
 }
 
-title.addEventListener('click', handleClick);
+title.addEventListener('click', handleClick);               // Add the click event to the 'handleClicl()' function
