042a04c0bd58abf1ae179dcb79bea660ec0cfba5
Spacery <msa.mt314@gmail.com>
2025-08-30 23:57:45 -0400

    (background, content): browser.* api

--- my-first-extension/popup/popup.js
+++ my-first-extension/popup/popup.js
@@ -1,5 +1,9 @@
-const title = document.getElementById('title');
+const title = document.getElementById('title');             // Gets the 'title' h1 reference
 
-title.addEventListener('click', () => {
-    title.textContent = 'It works!';
+title.addEventListener('click', () => {                     // Add a 'click' in 'title' h1 reference
+    console.log("Title clicked. Sending message.");
+
+    browser.runtime.sendMessage({                           // Send a msg to other parts of the extension
+        action: "changeColor"
+    });
 });
