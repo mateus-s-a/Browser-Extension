f1f825635fe26ef0a745f085670d1d1673049eb2
Spacery <msa.mt314@gmail.com>
2025-09-01 15:42:27 -0400

    fix(download): Correctly parse API object response and open download tab

--- youtube-audio-downloader/popup/popup.js
+++ youtube-audio-downloader/popup/popup.js
@@ -40,30 +40,30 @@ async function handleDownload() {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-            'X-RapidAPI-Key': 'MY_API',
+            'X-RapidAPI-Key': '06873e9a9dmsh5690bf7dfbb2161p1dd49ejsn9c64de50c7a6',
             'X-RapidAPI-Host': 'tube-mp31.p.rapidapi.com'
         },
         body: JSON.stringify({ videoId: videoId })
     };
 
     try {
-        console.log(fetchOptions);
-        
         const response = await fetch('https://tube-mp31.p.rapidapi.com/api/json', fetchOptions);
-        console.log(response);
-
         const result = await response.json();
-        console.log(result);
 
         if (result.status === 'success' && result.result && result.result[0].dlurl) {
             const downloadUrl = result.result[0].dlurl;
             console.log(`Success. Download link received: ${downloadUrl}`);
 
-            const sanitizedTitle = videoTitleElement.innerText.replace(/[/\\?%*:|"<>]/g, '_');
+            /*const sanitizedTitle = videoTitleElement.innerText.replace(/[/\\?%*:|"<>]/g, '_');
             browser.downloads.download({
                 url: downloadUrl,
                 filename: `${sanitizedTitle}.mp3`
+            });*/
+
+            browser.tabs.create({
+                url: downloadUrl
             });
+
         } else {
             console.error("API did not return a success status or download link", result);
         }
